ef731f3490e3320d1dc396ebcbd34b4ed8177624
dpa0702 <56276309+dpa0702@users.noreply.github.com>
2021-02-01 17:01:23 -0300

    Carga Inicial
    Carga Inicial

---
+++ SuperHeroi/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SuperHeroi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int a = 0; a < 5; a++)
+            {
+                Voar(a);
+            }
+        }
+
+        public static void Voar(int v)
+        {
+            int voar = v;
+
+            var sHeroi = new SuperHeroi()
+            {
+                nome = "Super Man",
+                nascimento = DateTime.Now.AddYears(-40),
+                kryptonita = voar
+            };
+
+            if (sHeroi.kryptonita < 2)
+            {
+                Console.WriteLine(sHeroi.kryptonita + " Voando...");
+            }
+            else
+            {
+                Console.WriteLine(sHeroi.kryptonita + " Não Voando...");
+            }
+        }
+        public class SuperHeroi
+        {
+            public string nome { get; set; }
+            public DateTime nascimento { get; set; }
+            public int kryptonita { get; set; }
+        }
+    }
+}
---
+++ SuperHeroi/SuperHeroi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace SuperHeroi
+{
+    public class SuperHeroi
+    {
+        public SuperHeroi()
+        {
+        }
+
+        public string nome { get; set; }
+        public DateTime nascimento { get; set; }
+        public int kryptonita { get; set; }
+    }
+}
